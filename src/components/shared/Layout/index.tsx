6a8ec4806bbe4978fde78d526f4827b8b2b45103
sercan turkoglu <sercan.turkoglu@silverrailtech.com>
2023-02-10 16:54:02 +0000

    feat(): homepage and footer fix

--- src/components/shared/Layout/index.tsx
+++ src/components/shared/Layout/index.tsx
@@ -3,9 +3,9 @@ import Footer from '../Footer';
 import Header from '../Header';
 
 const Layout = ({ children }: { children: ReactNode }) => (
-  <main className="flex flex-col min-h-screen">
+  <main className="flex flex-col min-h-screen ">
     <Header />
-    <main className="container mx-auto flex-grow">{children}</main>
+    <main className="container mx-auto flex-grow py-4">{children}</main>
     <Footer />
   </main>
 );
