8d6eb8735dbc58029f711e490a2945c199379534
sercan turkoglu <sercan.turkoglu@silverrailtech.com>
2023-02-12 09:21:22 +0000

    fix(): placeholder

--- src/components/formElements/input/inputPhone.tsx
+++ src/components/formElements/input/inputPhone.tsx
@@ -1,18 +1,18 @@
 import { ChangeEvent, FC } from 'react';
 import InputMask from 'react-input-mask';
-import Input from '.';
+import Input, { IInputProps } from '.';
 
-interface IInputPhoneProps {
-  name: string;
+type IInputPhoneProps = {
   value?: string;
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
-}
+} & Pick<IInputProps, 'name' | 'onChange' | 'placeholder'>;
 
-const InputPhone: FC<IInputPhoneProps> = ({ name, onChange }) => (
+const InputPhone: FC<IInputPhoneProps> = ({ name, onChange, placeholder }) => (
   <InputMask
     name={name}
     mask="(999) 999 99 99"
     onChange={onChange}
+    placeholder={placeholder}
   >
     {(inputProps) => <Input {...inputProps} />}
   </InputMask>
