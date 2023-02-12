0104808986896a3ed6bbbaf12594d0bef1770b1a
sercan turkoglu <sercan.turkoglu@silverrailtech.com>
2023-02-12 18:24:59 +0000

    fix(): typo

--- src/pages/Case.tsx
+++ src/pages/Case.tsx
@@ -87,10 +87,10 @@ const Case = () => {
         <div className="border-t border-gray-200 text-center pt-8">
           <h1 className="text-8xl lg:text-9xl font-bold text-black">404</h1>
           <h1 className="text-3xl lg:text-6xl font-medium py-8">
-            Taleb Bulunamadi.
+            Talep Bulunamadi.
           </h1>
           <p className="text-1xl pb-8 px-12 font-medium">
-            Taleb tamamlanmis veya iptal edilmis olabilir..
+            Talep tamamlanmis veya iptal edilmis olabilir..
           </p>
           <div className="flex flex-col md:flex-row gap-2 justify-center items-center">
             <Link
