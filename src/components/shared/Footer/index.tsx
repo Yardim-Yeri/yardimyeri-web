6ed53059d3c66070ab81d54ed65377b190c5e9e5
sercan turkoglu <sercan.turkoglu@silverrailtech.com>
2023-02-15 21:02:47 +0000

    feat: kvkk ve gizlilik sozlesmesi pages

--- src/components/shared/Footer/index.tsx
+++ src/components/shared/Footer/index.tsx
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const Footer = () => (
-  <footer className="h-14 flex justify-center items-center flex-col sm:flex-row gap-4 text-sm p-4 mt-6">
-    <div>
+  <footer className="h-14 flex flex-col justify-center items-centergap-4 text-sm p-4 mt-6">
+    <div className="flex flex-col justify-center gap-2 md:flex-row md:gap-8 mb-8 font-bold">
+      <div className="flex justify-center">
+        <Link to="/kvkk">Aydınlatma Metni</Link>
+      </div>
+      <div className="flex justify-center">
+        <Link to="/gizlilik-sozlesmesi">Gizlilik Politikasi</Link>
+      </div>
+    </div>
+    <div className="flex justify-center">
       Geri Bildirim:
       <a
         href="mailto:yardimyeri.info@gmail.com"
@@ -11,7 +21,7 @@ const Footer = () => (
         Mail Gönder
       </a>
     </div>
-    <div> © 2023 YARDIMYERİ.COM</div>
+    <div className="flex justify-center">© 2023 YARDIMYERİ.COM</div>
   </footer>
 );
 
