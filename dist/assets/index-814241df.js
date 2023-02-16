421aa27ffcd743f05a877d8fee196f04e744c9fe
Sercan TÜRKOĞLU <sercanturkoglu67@gmail.com>
2023-02-16 00:33:19 +0000

    build (#50)
    Co-authored-by: sercan turkoglu <sercan.turkoglu@silverrailtech.com>

---
+++ dist/assets/index-814241df.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-f32199b4.js";const l=({title:t})=>e("div",{children:e("h1",{className:"text-3xl font-bold py-5",children:t})});export{l as P};
--- dist/assets/index-fd076e3a.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-62296d16.js";const l=({title:t})=>e("div",{children:e("h1",{className:"text-3xl font-bold py-5",children:t})});export{l as P};
